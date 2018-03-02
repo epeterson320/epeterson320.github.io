11fb197bef3fa2144f30b306b4d1da28f30f6c1c
Eric Peterson <epeterson@cognosante.com>
2018-03-02 17:46:30 -0500

    Add forward-back links

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import g from 'glamorous';
+import Link from 'gatsby-link';
 
 import Main from '../components/Main';
 import Headline from '../components/Headline';
 import ContactForm from '../components/ContactForm';
 import { rhythm } from '../utils/typography';
 
-
 export default ({ data }) => {
   const { html, frontmatter, fields } = data.markdownRemark;
+  const { date, datetime, slug } = fields;
+  const { edges } = data.allMarkdownRemark;
+  const { next, previous } = edges.find(edge => edge.node.fields.slug === slug);
   return (
     <Main>
       <Headline>{frontmatter.title}</Headline>
@@ -17,16 +20,33 @@ export default ({ data }) => {
         textAlign="center"
         marginBottom={rhythm(3)}
       >
-        <time dateTime={fields.datetime}>{fields.date}</time>
+        <time dateTime={datetime}>{date}</time>
       </g.Div>
       {/* eslint-disable react/no-danger */}
       {/* it's recommended in gatsbyjs.org/docs/adding-markdown-pages */}
       <div dangerouslySetInnerHTML={{ __html: html }} />
       {/* eslint-enable react/no-danger */}
+      {previous ? (
+        <PrevNextLink to={previous.fields.slug}>
+          Previous: {previous.frontmatter.title}
+        </PrevNextLink>
+      ) : null}
+      {next ? (
+        <PrevNextLink to={next.fields.slug}>
+          Next: {previous.frontmatter.title}
+        </PrevNextLink>
+      ) : null}
       <ContactForm />
     </Main>
   );
 };
+
+const PrevNextLink = g(Link)({
+  display: 'inline-block',
+  textDecoration: 'none',
+  width: '15em',
+  margin: rhythm(0.5),
+});
 
 export const query = graphql`
   query BlogPostQuery($slug: String!) {
@@ -36,8 +56,34 @@ export const query = graphql`
         title
       }
       fields {
+        slug
         date: date(formatString: "MMMM DD, YYYY")
         datetime: date
+      }
+    }
+    allMarkdownRemark(sort: { fields: [fields___date], order: ASC }) {
+      edges {
+        previous {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+        next {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+        node {
+          fields {
+            slug
+          }
+        }
       }
     }
   }
