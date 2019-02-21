06af6420e05477c4f97874aea99320f2045818e3
Eric Peterson <epeterson@cognosante.com>
2019-02-21 16:22:29 -0500

    Refactor blog post

--- src/components/blog-post.js
+++ src/components/blog-post.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-const BlogPost = ({ pageContext }) => {
-  const { node, next, previous } = pageContext;
-  const { html, frontmatter, fields } = node;
-  const { date, datetime } = fields;
-
-  return (
-    <main>
-      <h1>{frontmatter.title}</h1>
-      <div>
-        <time dateTime={datetime}>{date}</time>
-      </div>
-      {/* eslint-disable react/no-danger */}
-      {/* it's recommended in gatsbyjs.org/docs/adding-markdown-pages */}
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-      {/* eslint-enable react/no-danger */}
-      {previous ? (
-        <Link to={previous.fields.slug}>
-          Previous: {previous.frontmatter.title}
-        </Link>
-      ) : null}
-      {next ? (
-        <Link to={next.fields.slug}>
-          Next: {next.frontmatter.title}
-        </Link>
-      ) : null}
-    </main>
-  );
-};
+const BlogPost = ({
+  pageContext: {
+    node: { html, frontmatter, fields },
+    next,
+    previous,
+  },
+}) => (
+  <main>
+    <h1>{frontmatter.title}</h1>
+    <div>
+      <time dateTime={fields.datetime}>{fields.date}</time>
+    </div>
+    {/* eslint-disable react/no-danger */}
+    {/* it's recommended in gatsbyjs.org/docs/adding-markdown-pages */}
+    <div dangerouslySetInnerHTML={{ __html: html }} />
+    {/* eslint-enable react/no-danger */}
+    {previous ? (
+      <Link to={previous.fields.slug}>
+        Previous: {previous.frontmatter.title}
+      </Link>
+    ) : null}
+    {next ? (
+      <Link to={next.fields.slug}>
+        Next: {next.frontmatter.title}
+      </Link>
+    ) : null}
+  </main>
+);
 
 export default BlogPost;
