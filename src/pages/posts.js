16d3e8e8612c1032bdd4cf4546f5e676baa4d59f
Eric Peterson <epeterson@cognosante.com>
2019-02-21 16:32:11 -0500

    Add posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,3 +1,43 @@
-const AllPostsPage = () => null;
+import React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
+
+const AllPosts = (props) => (
+  <main>
+    <h1>All Posts</h1>
+    {props.data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.fields.slug}>
+        <h1>{node.frontmatter.title}</h1>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
+  </main>
+);
+
+const AllPostsPage = () => (
+  <StaticQuery
+    query={query}
+    render={(data) => <AllPosts data={data} />}
+  />
+);
 
 export default AllPostsPage;
+
+const query = graphql`
+  query PostsQuery {
+    allMarkdownRemark(sort: { fields: [fields___date], order: DESC }) {
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+            date: date(formatString: "MMMM DD, YYYY")
+            datetime: date
+          }
+          excerpt(pruneLength: 200)
+        }
+      }
+    }
+  }
+`;
