0f04c836c7266114bc4b97959d149e7110c4cd5d
Eric Peterson <git@ericp.co>
2019-03-04 11:16:17 -0500

    Footer perfectly responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 export default () => (
-  <footer className="bg-grey-darkest text-white h-24 mt-16 flex flex-wrap items-end justify-end">
-    <span className="flex-1" />
-    <p className="block">
+  <footer className="bg-grey-darkest text-white h-24 mt-16 flex flex-col justify-end items-end md:flex-row md:justify-center">
+    <p className="self-center md:self-end">
       This part is the website footer. Thanks for reading.
     </p>
-    <span className="text-xs pr-1 flex-1 text-right">
+    <span className="text-xs md:absolute pin-r">
       © {new Date().getFullYear()}, Built with&nbsp;
       <a className="text-orange-lighter" href="https://www.gatsbyjs.org">
         Gatsby
