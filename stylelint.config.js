3db3c15e472424e029a68e60790cccca7c733749
Eric Peterson <eric.peterson@cognosante.com>
2019-03-18 12:17:16 -0400

    Autoformat w prettier

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,5 +4,8 @@ module.exports = {
     // These don't work well with jsx
     'declaration-block-trailing-semicolon': null,
     'declaration-colon-space-after': null,
+    // Doesn't work well with prettier
+    'declaration-colon-newline-after': null,
+    'value-list-comma-newline-after': null,
   },
 };
