0cee49a0397b6d5f50af95a5351ed70b721d1527
Eric Peterson <eric.peterson@cognosante.com>
2019-03-21 11:01:28 -0400

    Resize header tools responsively

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,83 +1,50 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import { GitHubIcon, RssIcon, EmailIcon, CodePenIcon } from './icons';
+import { GitHubIcon, RssIcon, EmailIcon, CodePenIcon, BarsIcon } from './icons';
+import styles from './NavHeader.module.css';
 
 const NavHeader = () => (
-  <header
-    style={{
-      borderBottom: '1px solid silver',
-      backgroundColor: 'aliceblue',
-      display: 'flex',
-      justifyContent: 'center',
-    }}
-  >
-    <nav
-      style={{
-        maxWidth: '60rem',
-        display: 'flex',
-        width: '100%',
-        padding: '0 0.5rem',
-      }}
-    >
-      <h2
-        style={{
-          fontWeight: 'bold',
-          padding: '0.4rem 0',
-          alignSelf: 'start',
-        }}
-      >
-        <Link to="/">Eric Peterson</Link>
-      </h2>
-      <ul
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          flex: '1',
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-        }}
-      >
-        <ListLink to="/posts">Blog</ListLink>
-        <ListLink to="https://www.github.com/epeterson320" external>
-          <GitHubIcon />
-          &nbsp;Github
-        </ListLink>
-        <ListLink to="https://codepen.io/epeterson320/" external>
-          <CodePenIcon />
-          &nbsp;CodePen
-        </ListLink>
-        <ListLink to="/contact">
-          <EmailIcon />
-          &nbsp;Contact
-        </ListLink>
-        <ListLink to="/rss.xml" label="RSS" external>
-          <RssIcon />
-        </ListLink>
-      </ul>
+  <header className={styles.header}>
+    <h2 className={styles.title}>
+      <Link to="/">Eric Peterson</Link>
+    </h2>
+    <input id="nav-links-toggle" type="checkbox" className={styles.toggle} />
+    <label htmlFor="nav-links-toggle" className={styles.toggleLabel}>
+      <BarsIcon />
+    </label>
+    <nav className={styles.nav} role="navigation">
+      <ListLink to="/posts">Blog</ListLink>
+      <ListLink to="https://www.github.com/epeterson320" external>
+        <GitHubIcon />
+        &nbsp;Github
+      </ListLink>
+      <ListLink to="https://codepen.io/epeterson320/" external>
+        <CodePenIcon />
+        &nbsp;CodePen
+      </ListLink>
+      <ListLink to="/contact">
+        <EmailIcon />
+        &nbsp;Contact
+      </ListLink>
+      <ListLink to="/rss.xml" label="RSS" external>
+        <RssIcon />
+      </ListLink>
     </nav>
   </header>
 );
 
 export default NavHeader;
 
-const linkStyle = {
-  textDecoration: 'none',
-  color: 'black',
-  padding: '0.5rem',
-  display: 'flex',
-  alignItems: 'center',
-};
-
 const ListLink = ({ to, label, children, external = false }) => {
   const link = external ? (
-    <a href={to} aria-label={label} style={linkStyle}>
+    <a href={to} aria-label={label} className={styles.link}>
       {children}
     </a>
   ) : (
-    <Link to={to} aria-label={label} style={linkStyle}>
+    <Link to={to} aria-label={label} className={styles.link}>
       {children}
     </Link>
   );
 
-  return <li style={{ display: 'inline' }}>{link}</li>;
+  return link;
 };
--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faCodepen } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { faRss } from '@fortawesome/free-solid-svg-icons';
+import { faRss, faBars } from '@fortawesome/free-solid-svg-icons';
 
-export const GitHubIcon = () => <FontAwesomeIcon icon={faGithub} />;
+export const BarsIcon = () => <FontAwesomeIcon icon={faBars} size="lg" />;
 export const CodePenIcon = () => <FontAwesomeIcon icon={faCodepen} />;
-export const RssIcon = () => <FontAwesomeIcon icon={faRss} />;
 export const EmailIcon = () => <FontAwesomeIcon icon={faEnvelope} />;
+export const GitHubIcon = () => <FontAwesomeIcon icon={faGithub} />;
+export const RssIcon = () => <FontAwesomeIcon icon={faRss} />;
